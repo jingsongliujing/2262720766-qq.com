7db545885600a4e78f3e819da039fd88335b29c3
jingsongliu <45508593+jingsongliujing@users.noreply.github.com>
2020-03-26 10:03:37 +0800

    Update 整数分拆.c

--- 整数分拆.c
+++ 整数分拆.c
@@ -1,1 +1,29 @@
+#include"stdio.h"
+#include"stdlib.h"
 
+int f(int n)
+{
+  int i;
+  int sum=0;
+  if(n==1){
+    return 1;
+  }
+  for(i=1;i<n;i++)
+  {
+    sum+=f(n-i);
+  }
+  sum++;
+  return sum;
+}
+
+int main(int argc,char *argv[])
+{
+  int n;
+  if(argc<2)
+  {
+    printf("need a position interger\n");
+    return  EXIT_FAILURE;
+    
+  }
+  printf("f(%d)=%d\n",n,f(n));
+}
