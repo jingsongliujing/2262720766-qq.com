11166506dfaee17fb14656a0b33f74bd4d247487
jingsongliu <45508593+jingsongliujing@users.noreply.github.com>
2020-03-30 23:28:35 +0800

    Update 数据结构列表.java

--- 数据结构列表.java
+++ 数据结构列表.java
@@ -1,1 +1,24 @@
+package org.lanqiao.algo.elementary._09_Line.list;
 
+public class MyArroylist implenents MyList{
+  private int size;
+  pravate Object[] element;
+  private int size=0;
+  private int copocity=10;
+  public MyArrayList(int capocity){
+  this.capocity=capacity;
+    elements=new Object[capacity];
+  }
+  
+  @Override
+  public void add(Object element){
+  }
+  
+  @Override
+  public void delete(Object element){
+  }
+  
+  @Override 
+  public void delect(int index){
+  }
+  
