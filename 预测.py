35ac1b3e36ee3436cf3eb6f5deac06b5fd0e10f2
jingsongliu <45508593+jingsongliujing@users.noreply.github.com>
2020-03-31 18:07:20 +0800

    Update 预测.py

--- 预测.py
+++ 预测.py
@@ -1,1 +1,30 @@
+y_pred=linreg.predict(X_test)
+print(y_pred)
+print(type(y_pred))
+
+print(type(y_pred),type(y_test))
+print(len(y_pred),len(y_test))
+print(y_pred.shape,y_test.shape)
+from sklearn import metrics
+import numpy as np
+sum_mean=0
+for i in range(len(y_pred)):
+  sum_mean+=(y_pred[i]-y_test.values[i])**2
+sum_erro=np.sqrt(sum_mean/50)
+print("rmse by band:",sum_erro)
+
+
+import matplotlib.pyplot as plt
+plt.figure()
+plt.plot(range(len(y_pred)),y_pred,'b',label="predict")
+plt.plot(range(len(y_pred)),y_test,'r',label="test")
+plt.legend(loc="upper right")
+plt.xlabel("the number of sales")
+plt.ylabel('value of sales')
+plt.show()
+
+
+
+
+
 
